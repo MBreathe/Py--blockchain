c354b3c8ffc56a520ccf7a25adead167398d7c74
Ilya Bubnov <makebreathe1224@gmail.com>
2026-01-27 17:57:36 +0100

    blockchain is almost built

--- blockchain.py
+++ blockchain.py
@@ -1,25 +1,60 @@
+import decimal
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+    def proof_of_work(self, last_proof:int)->int:
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
-    def new_transaction(self):
-        # adds a new transaction to the transaction list
-        pass
+        return proof
+
+    def new_block(self, proof: int, previous_hash:str=None)->dict:
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender: str, recipient: str, amount: decimal.Decimal)->int:
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
-    def hash(block):
-        # hashes the block
-        pass
+    def hash(block:dict)->str:
+        block_string = json.dumps(block, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
+
+    @staticmethod
+    def valid_proof(last_proof:int, proof:int)->bool:
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
 
     @property
-    def last_block(self):
-        # returns the last block in chain
-        pass
+    def last_block(self)->dict:
+        return self.chain[-1]
 
 
 # block = {
